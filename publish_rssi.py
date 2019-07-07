060ead66784f5cbb5f966d1f4a2936ada9ff092d
Kyler Freas <kmf5285@rit.edu>
2019-07-07 04:49:44 +0100

    added journald logging

--- publish_rssi.py
+++ publish_rssi.py
@@ -1,17 +1,31 @@
 """
 Bluetooth device proximity tracking.
 Monitors the RSSI received from bluetooth devices and 
-sends to HomeAssistant's Mosquitto instance.
+publishes to HomeAssistant's Mosquitto instance.
 """
+from systemd.journal import JournalHandler
 from bt_proximity import BluetoothRSSI
 import paho.mqtt.client as mqtt
+import logging
 import socket
 import json
 import time
 import sys
+import os
 
 # TODO Move to a database!!!
 BT_ADDR = 'C4:98:80:35:B6:1D'
+
+# Configure logging
+log = logging.getLogger(__name__)
+
+journald_handler = JournalHandler()
+journald_handler.setFormatter(logging.Formatter(
+    '[%(levelname)s] %(message)s'
+))
+
+log.addHandler(journald_handler)
+log.setLevel(logging.DEBUG)
 
 # MQTT client connection details
 broker = '192.168.1.5'
@@ -22,23 +36,21 @@ client.username_pw_set("kyler", "*()IOP890iop")
 # MQTT publish details
 client_topic = "pressence/{}/kyler".format(client_name)
 publish_interval = 1
-print(client_topic)
 
 # RSSI averaging params
 num_rssi_samples = 10  # Number of samples collected
 rssi_delay       = 0.5 # Delay between measurements (sec)
-connect_timeout  = 2   # BT connection timeout (sec)
 
 # MQTT connection callback
 def on_connect(client, userdata, flags, rc):
-    print("Connected flags " + str(flags) + ", result code "+str(rc)+", client1_id ")
+    log.info("Connected to HASS: flags={0}, result_code={1}, client_id={2}".format(str(flags), str(rc), client_name))
 
 # MQTT data published callback
-def on_publish(client, userdata, result):
-    print("data published \n")
+def on_disconnect(client, userdata,rc=0):
+    log.error("Disconnected from HASS with result code {}".format(str(rc)))
 
 client.on_connect = on_connect
-client.on_publish = on_publish
+client.on_disconnect = on_disconnect
 
 def main():
     # Connect to HomeAssistant
@@ -52,7 +64,6 @@ def main():
     while True:
         # Attempt connection to device and query RSSI
         btrssi = BluetoothRSSI(addr=BT_ADDR)
-        #btrssi.bt_sock.settimeout(connect_timeout)
         rssi = btrssi.get_rssi()
 
         # Default "away" value
@@ -63,11 +74,6 @@ def main():
         rssi_samples[sample_num] = rssi
         sample_num = (sample_num + 1) % num_rssi_samples
         avg_rssi = sum(rssi_samples) / num_rssi_samples
-
-        # Prepare data packet and publish
-        packet = {
-            'rssi': avg_rssi
-        }
 
         client.publish(client_topic, avg_rssi)
         time.sleep(rssi_delay)
